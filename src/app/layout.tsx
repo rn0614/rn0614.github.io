6c190b821c9b0370fca67aedf98ccda6b9807e3a
rn0614 <rn0614@naver.com>
2025-02-02 15:36:09 +0900

    Revert "fix: 외부 mdx style link가 body안에 있어서 가져오기 전에 렌더링하는 문제"
    This reverts commit 159be69fb7996e6ce5e1c03950ad7e18ff2a4f18.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { RecoilRoot } from "recoil";
 import RecoilProvider from "@/Provider/RecoilProvider";
 
 const inter = Inter({ subsets: ["latin"] });
