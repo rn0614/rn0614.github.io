70b44e738fcf6c13b451a617a89ae847d7cf25a8
rn0614 <rn0614@naver.com>
2025-02-02 15:38:04 +0900

    fix: 이전에서 Head집어넣으면 적용 안됨. next Head는 일반 Head와 다름

--- src/app/(template)/layout.tsx
+++ src/app/(template)/layout.tsx
@@ -2,17 +2,14 @@ import Aside from "@/components/Aside/Aside";
 import styles from "./layout.module.scss";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Head from "next/head";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div lang="en" className={styles.layout}>
-      <Head>
-        <link
-          href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css"
-          rel="stylesheet"
-        ></link>
-      </Head>
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.css"
+        rel="stylesheet"
+      ></link>
       <Header />
       <div className={styles.body}>
         <div className={styles.asideWrapper}>
