40fee18daaf6715efe5ddcca51c1baf8b947768b
rn0614 <rn0614@naver.com>
2025-02-04 23:58:28 +0900

    feat: header 햄버거 버튼 추가 이동 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,53 @@
-"use client"
+"use client";
 import React from "react";
 import withMobileVisibility from "../hoc/withMobileVisibility";
-import styles from './style.module.scss';
+import styles from "./style.module.scss";
+import { FaAlignJustify } from "react-icons/fa";
+import { Button, Container, DropdownMenu, Flex } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 
 function Header() {
-  return <header className={styles.header}>개발자 구상모 블로그</header>;
+  const router = useRouter();
+  return (
+    <Container asChild className={styles.header}>
+      <header>
+        <Flex justify={"between"}>
+          <a onClick={()=>router.push("/")}>log</a>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Button color="gray" variant="ghost" className={styles.dropdownMenu__button}>
+                <FaAlignJustify size={30} />
+              </Button>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Item className={styles.dropdownMenu__item} onClick={()=>router.push("/")}>
+                Home
+              </DropdownMenu.Item>
+              <DropdownMenu.Sub>
+                <DropdownMenu.SubTrigger className={styles.dropdownMenu__item}>
+                  Code
+                </DropdownMenu.SubTrigger>
+                <DropdownMenu.SubContent>
+                  <DropdownMenu.Item className={styles.dropdownMenu__item} onClick={()=>router.push("/category/dev")}>
+                    개발사항
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item className={styles.dropdownMenu__item} onClick={()=>router.push("/category/etc")}>
+                    기타
+                  </DropdownMenu.Item>
+                </DropdownMenu.SubContent>
+              </DropdownMenu.Sub>
+              <DropdownMenu.Item className={styles.dropdownMenu__item}>
+                일과
+              </DropdownMenu.Item>
+              <DropdownMenu.Item className={styles.dropdownMenu__item}>
+                일정
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </Flex>
+      </header>
+    </Container>
+  );
 }
 
 export default withMobileVisibility(Header, null);
