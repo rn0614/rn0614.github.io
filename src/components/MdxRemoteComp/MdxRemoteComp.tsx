bc999a4a653ac1cc5a341ba9cd4133f9c63806e7
rn0614 <rn0614@naver.com>
2025-02-12 21:18:28 +0900

    feat: markdown 문서 Heading 한단계 줄이기

--- src/components/MdxRemoteComp/MdxRemoteComp.tsx
+++ src/components/MdxRemoteComp/MdxRemoteComp.tsx
@@ -3,7 +3,12 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import remarkGfm from 'remark-gfm'
 const components = {
   img: (props: any) => <img {...props} src={props.src.replace("public", "")} />, //public 주소만 제외
-  a:(props:any) => <a {...props} href={`/${props.href}`}/>
+  a:(props:any) => <a {...props} href={`/${props.href}`}/>,
+  h1:(props:any) => <h2 {...props}/>,
+  h3:(props:any) => <h3 {...props}/>,
+  h4:(props:any) => <h4 {...props}/>,
+  h5:(props:any) => <h5 {...props}/>,
+  h6:(props:any) => <h6 {...props}/>
 };
 
 export default function MdxRenderer({
