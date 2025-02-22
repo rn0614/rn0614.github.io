d7f1290617ed633a7d13046553a967063951bbea
rn0614 <rn0614@naver.com>
2025-02-22 23:55:28 +0900

    fix: robots allow 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "*",
+        allow: "/",
         disallow: ["/admin/*"],
       },
     ],
