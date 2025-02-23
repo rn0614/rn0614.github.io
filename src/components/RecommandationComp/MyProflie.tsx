7e8943631bcaeb9c64d788035b290af596ff1c9f
rn0614 <rn0614@naver.com>
2025-02-23 20:31:23 +0900

    perf: 소개글 스타일 개선작업
    heading 및 p 간격 조정

--- src/components/RecommandationComp/MyProflie.tsx
+++ src/components/RecommandationComp/MyProflie.tsx
@@ -5,15 +5,23 @@ import Hr from "../Hr/Hr";
 export default function MyProflie() {
   return (
     <Section py="3">
-      <Flex direction={{ initial:"column", sm: "row" }} justify={"between"} gap="7" >
+      <Flex
+        direction={{ initial: "column", sm: "row" }}
+        justify={{ initial: "between", sm: "start" }}
+        gap={{ initial: "5", sm: "7" }}
+      >
         <Container
-          style={{ display: "flex", flexDirection: "column", flex: 1 }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
         >
           <Box
             style={{
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
+              padding: "1rem",
             }}
           >
             <img
@@ -24,58 +32,81 @@ export default function MyProflie() {
           </Box>
           <ul>
             <li>
-              <Heading as="h3">프로필</Heading>
+              <Heading as="h3" size="6">
+                프로필
+              </Heading>
               <Text>Tel : 010-9502-8766</Text>
               <Text>Mail : 010-9502-8766</Text>
               <Hr />
             </li>
             <li>
-              <Heading as="h3">학력</Heading>
+              <Heading as="h3" size="6">
+                학력
+              </Heading>
               <Text>2014.02 ~ 2020.02</Text>
               <Text>인하대학교 기계공학과</Text>
               <Hr />
             </li>
             <li>
-              <Heading as="h3">GITHUB</Heading>
+              <Heading as="h3" size="6">
+                GITHUB
+              </Heading>
               <Text>https://github.com/rn0614</Text>
               <Hr />
             </li>
             <li>
-              <Heading as="h3">BLOG</Heading>
+              <Heading as="h3" size="6">
+                BLOG
+              </Heading>
               <Text>https://rn0614.github.io</Text>
             </li>
           </ul>
         </Container>
         <Container
-          style={{ display: "flex", flexDirection: "column", flex: 2 }}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+          }}
         >
           <ul>
             <li>
-              <Heading as="h3">경력</Heading>
+              <Heading as="h3" size="6">
+                경력
+              </Heading>
               <Text>
                 현대오토에버 :SI /SM 개발자 2022.04 ~ 현재(2년 10개월)
               </Text>
               <Hr />
             </li>
             <li>
-              <Heading as="h3">SKILL</Heading>
-              <Heading as="h4">1. FrontEnd</Heading>
+              <Heading as="h3" size="6">
+                SKILL
+              </Heading>
+              <Heading as="h4" size="4">
+                1. FrontEnd
+              </Heading>
               <Flex direction={"column"} pb="4" pl="3">
                 <Text>React, Nextjs</Text>
                 <Text>Sass, styled-component</Text>
                 <Text>js, jquery</Text>
               </Flex>
-              <Heading as="h4">2. BackEnd</Heading>
+              <Heading as="h4" size="4">
+                2. BackEnd
+              </Heading>
               <Flex direction={"column"} pb="4" pl="3">
                 <Text>Spring Boot</Text>
                 <Text>FastApi</Text>
               </Flex>
-              <Heading as="h4">3. 모바일</Heading>
+              <Heading as="h4" size="4">
+                3. 모바일
+              </Heading>
               <Flex direction={"column"} pb="4" pl="3">
                 <Text>react-native</Text>
                 <Text>android</Text>
               </Flex>
-              <Heading as="h4">4. CI/CD</Heading>
+              <Heading as="h4" size="4">
+                4. CI/CD
+              </Heading>
               <Flex direction={"column"} pb="4" pl="3">
                 <Text>gitlab</Text>
                 <Text>github action</Text>
--- src/components/RecommandationComp/SelfRecomandation.tsx
+++ src/components/RecommandationComp/SelfRecomandation.tsx
@@ -1,25 +1,29 @@
-import { Heading, Section, Text } from "@radix-ui/themes";
+import { Box, Heading, Section, Text } from "@radix-ui/themes";
 
 export default function SelfRecomandation() {
   return (
     <Section py="4" style={{ display: "flex", flexDirection: "column" }}>
-      <Heading as="h3">자기소개글</Heading>
-      <Text as="p">안녕하세요, 4년차 엔지니어 구상모입니다</Text>
-      <Text as="p">
-        모든 코드는 문제를 해결하기 위한 수단이라고 생각하고 내가 작성하는
-        코드가 어떤문제를 해결하는지에 관심을 갖습니다.
-      </Text>
-      <Text as="p">
-        SM인력의 유지보수 용이성 및 고객의 사용편의성을 중점적으로 생각합니다.
-      </Text>
-      <Text as="p">
-        문제상황을 발견하고 이에 맞는 좋은 방식으로 코드를 개선하는 걸
-        좋아합니다.
-      </Text>
-      <Text as="p">
-        현재는 팀 내에서 AI프로젝트, 공장 시스템 유지보수 업무를 담당하고
-        있습니다.
-      </Text>
+      <Heading as="h3" size="6">
+        자기소개글
+      </Heading>
+      <Box mt="10px">
+        <Text as="p">안녕하세요, 3년차 엔지니어 구상모입니다</Text>
+        <Text as="p">
+          모든 코드는 문제를 해결하기 위한 수단이라고 생각하고 내가 작성하는
+          코드가 어떤문제를 해결하는지에 관심을 갖습니다.
+        </Text>
+        <Text as="p">
+          SM인력의 유지보수 용이성 및 고객의 사용편의성을 중점적으로 생각합니다.
+        </Text>
+        <Text as="p">
+          문제상황을 발견하고 이에 맞는 좋은 방식으로 코드를 개선하는 걸
+          좋아합니다.
+        </Text>
+        <Text as="p">
+          현재는 팀 내에서 AI프로젝트, 공장 시스템 유지보수 업무를 담당하고
+          있습니다.
+        </Text>
+      </Box>
     </Section>
   );
 }
