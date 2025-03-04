29e84c58624fc8959301302f0bcccdbd5fda3aa0
rn0614 <rn0614@naver.com>
2025-03-05 00:42:59 +0900

    fix: next-intl 다국어지원테스트

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const nextConfig = withNextIntl({
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   output: "export",
   compiler: {
@@ -11,6 +11,6 @@ const nextConfig = {
         ? { exclude: ["error", "warn"] }
         : false,
   },
-};
+});
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
