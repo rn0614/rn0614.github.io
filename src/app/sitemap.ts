96327ec7c53bd97ffae8589dfce2de9770a67a6d
rn0614 <rn0614@naver.com>
2025-04-04 18:57:05 +0900

    fix sitemap 수정정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,14 +2,14 @@ import { getAllPostsList } from "@/lib/posts";
 import dayjs from "dayjs";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL as string;
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://rn0614.github.io';
 
   const posts = getAllPostsList();
 
   const blogPages = posts.map((post:any) => {
     return {
-      url: encodeURI(`${baseUrl}/${post.slug.replace(/\\/g, "/").replace(/\.md$/, "")}`),
+      url: `${baseUrl}/${post.slug.replace(/\\/g, "/").replace(/\.md$/, "")}`,
       lastModified: dayjs(
         post.metadata?.last_modified_at || "2025/01/01 00:00:00",
         "YYYY/MM/DD HH:mm:ss"
@@ -22,7 +22,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: dayjs().format("YYYY-MM-DD"),
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1
     },
